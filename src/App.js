d75e8a50a82641660afb663af1ed9debec969032
Kevin Tan <kevin.tan@liferay.com>
2018-02-27 21:44:09 -0800

    Add router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to
-					reload.
-				</p>
-			</div>
+			<Router>
+				<Fragment>
+					<Link to="/">Home</Link>
+
+					<Link to="/login">Log In</Link>
+
+					<Route exact path="/" component={Home} />
+
+					<Route path="/login" component={Login} />
+				</Fragment>
+			</Router>
 		);
 	}
 }
