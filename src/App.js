9f10cba9f4b5967d19b14f5bc62aac8fcf26337f
Kevin Tan <kevin.tan@liferay.com>
2018-03-01 21:44:59 -0800

    Revert "Add prettier"
    This reverts commit 82e02fe029d362f25316284b22bfa026804cf72e.

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import Login from './pages/Login';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Fragment>
-          <Link to="/">Home</Link>
+	render() {
+		return (
+			<Router>
+				<Fragment>
+					<Link to="/">Home</Link>
 
-          <Link to="/login">Log In</Link>
+					<Link to="/login">Log In</Link>
 
-          <Route exact path="/" component={Home} />
+					<Route exact path="/" component={Home} />
 
-          <Route path="/login" component={Login} />
-        </Fragment>
-      </Router>
-    );
-  }
+					<Route path="/login" component={Login} />
+				</Fragment>
+			</Router>
+		);
+	}
 }
 
 export default App;
