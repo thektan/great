70a346748e1b11eff82f5a0045bc41a52459d82e
Kevin Tan <kevin.tan@liferay.com>
2018-03-01 21:53:23 -0800

    Run prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component, Fragment } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-				<Fragment>
-					<Link to="/">Home</Link>
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <Link to="/">Home</Link>
 
-					<Link to="/login">Log In</Link>
+          <Link to="/login">Log In</Link>
 
-					<Route exact path="/" component={Home} />
+          <Route exact path="/" component={Home} />
 
-					<Route path="/login" component={Login} />
-				</Fragment>
-			</Router>
-		);
-	}
+          <Route path="/login" component={Login} />
+        </Fragment>
+      </Router>
+    );
+  }
 }
 
 export default App;
