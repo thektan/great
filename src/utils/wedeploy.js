58a0bc2f9180f608f581551db1d4ae8330da7cea
Kevin Tan <kevin.tan@liferay.com>
2018-03-22 21:23:10 -0700

    Authenticate requests with the current user

--- src/utils/wedeploy.js
+++ src/utils/wedeploy.js
@@ -4,4 +4,6 @@ export const AUTH = WeDeploy.auth("https://auth-great.wedeploy.io");
 
 export const { currentUser } = AUTH;
 
-export const DATA = WeDeploy.data("https://data-great.wedeploy.io");
+export const DATA = WeDeploy.data("https://data-great.wedeploy.io").auth(
+  currentUser
+);
